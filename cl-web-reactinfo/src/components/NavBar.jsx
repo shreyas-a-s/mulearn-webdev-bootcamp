fb6df2534e107f7a5b46452256a8b9733a0e6adf
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-08-21 22:55:13 +0530

    fix(cl-web-reactinfo): Fix for react icon not showing up in NavBar

--- cl-web-reactinfo/src/components/NavBar.jsx
+++ cl-web-reactinfo/src/components/NavBar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import reactSmallIcon from '../assets/react-icon-small.png';
 
 export default function Navbar() {
   return (
     <nav>
-      <img src="../assets/react-icon-small.png" className="nav--icon" alt="" />
+      <img src={reactSmallIcon} className="nav--icon" alt="" />
       <h3 className="nav--logo_text">ReactFacts</h3>
       <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
