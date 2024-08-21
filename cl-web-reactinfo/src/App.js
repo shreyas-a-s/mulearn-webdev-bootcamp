b868118d7f46ea10b771d53061a615ce79131917
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-08-21 22:35:12 +0530

    feat(cl-web-reactinfo): Create NavBar and Main components

--- cl-web-reactinfo/src/App.js
+++ cl-web-reactinfo/src/App.js
@@ -1,19 +1,11 @@
+import Navbar from "./components/NavBar";
+import Main from "./components/Main";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Main />
     </div>
   );
 }
