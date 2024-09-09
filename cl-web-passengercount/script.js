427309432857b9e9137d6b539de950f2349d4485
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-09-10 00:24:39 +0530

    feat(cl-web-passengercount): Implement counter functionality using js

---
+++ cl-web-passengercount/script.js
@@ -0,0 +1,21 @@
+let counter = 0;
+const countDisplay = document.getElementById('counter');
+const incBtn = document.getElementById('inc-btn');
+const saveBtn = document.getElementById('save-btn');
+const prevEnt = document.getElementById('prev-entr');
+
+const displayCount = () => {
+  countDisplay.textContent = counter;
+}
+
+const incrementCount = () => {
+  counter++;
+  displayCount();
+}
+
+const saveCount = () => {
+  prevEnt.textContent += `${counter} - `;
+}
+
+incBtn.addEventListener('click', incrementCount);
+saveBtn.addEventListener('click', saveCount);
